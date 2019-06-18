661e9becff82c8d9efd4a47fccdde336216b4078
luzhongkuan <luzhongk@126.com>
2019-06-18 16:20:12 +0800

    rename

--- build/buildLib.js
+++ build/buildLib.js
@@ -1,6 +1,7 @@
 const build = require('./build')
 const libConf = require('./webpack.lib.conf.js')
 const libMinConf = require('./webpack.lib.min.conf.js')
+const libExtract = require('./webpack.lib.extract.conf')
 
 const noop = config => config
 
@@ -12,11 +13,18 @@ function buildLib(extend = noop, config = libMinConf) {
   })
 }
 
-module.exports = async ({ extend = noop, onSuccess = noop, config } = {}) => {
+module.exports = async ({
+  extend = noop,
+  onSuccess = noop,
+  config,
+  extract = false
+} = {}) => {
   if (!config) {
     // 默认配置
     await buildLib(extend, libConf)
     await buildLib(extend, libMinConf)
+  } else if (extract) {
+    await buildLib(extend, libExtract)
   } else {
     // 自定义打包配置
     await buildLib(extend, config)
